f361ff2aa8a42b636a9c631af47eb50bd231e261
carlapveiga <31811607+carlapveiga@users.noreply.github.com>
2018-01-14 21:33:52 -0200

    Update challenge-20.js

--- challenge-20/challenge-20.js
+++ challenge-20/challenge-20.js
@@ -16,43 +16,54 @@
   nome, `username` deve receber "Desconhecido".
   Com a resposta, mostre um alert com a mensagem "Bem vindo [USERNAME]!"
   */
-  // ?
+  
+
+	var username = prompt('Qual seu nome?');
+	console.log(username)
+	if (!username)
+	username = 'Desconhecido';
+
+	
+	alert('Bem vindo ' + username);
 
   /*
   Agora, pergunte ao usuário "Qual o seu e-mail?", atribuindo o resultado à
   uma variável chamada `email`.
   */
-  // ?
+  
+	var email = prompt('Qual o seu email?');
 
   /*
   - Selecione o input de "Nome", atribuindo-o à uma variável chamada
   `$inputUsername`.
   */
-  // ?
+	$inputUsername = doc.querySelector('[type="text"]');
 
   /*
   - Selecione o input de "Email", atribuindo-o à uma variável chamada
   `$inputEmail`.
   */
-  // ?
+  
+	$inputEmail = doc.querySelector('[type="email"]');
 
   /*
   - Selecione o campo de "Mensagem", atribuindo-o à uma variável chamada
   `$message`.
   */
-  // ?
+	$message = doc.getElementsByName('textarea');
 
   /*
   - Selecione o botão de envio do formulário, atribuindo-o à uma variável
   chamada `$button`.
   */
-  // ?
+	$button = doc.querySelector('[type="email"]');
 
   /*
   Preencha os campos de "Nome" e "Email" que estão no documento com os valores
   entrados pelo usuário.
   */
-  // ?
+	$inputUsername.value = username;
+	$inputEmail.value = email;
 
   /*
   Adicione um listener de evento de click ao botão que faça o seguinte:
