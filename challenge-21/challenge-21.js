b7890065a84439542a118dc026adeffe898cff56
carlapveiga <31811607+carlapveiga@users.noreply.github.com>
2018-01-17 00:34:49 -0200

    Update challenge-21.js

--- challenge-21/challenge-21.js
+++ challenge-21/challenge-21.js
@@ -14,4 +14,52 @@ Utilize o atributo data-js para nomear o campo e os botões. Você pode
 usar o nome que achar melhor, desde que ele seja semântico, ou seja, o nome
 dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
 */
-// ?
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Challenge 21</title>
+
+</head>
+<body>
+	<input type="text" data-js="input"> 
+	<button type="submit" data-js="start">Start</button>
+	<button type="submit" data-js="stop">Stop</button>
+	<button type="submit" data-js="reset">Reset</button>
+    <script src="challenge-21.js"></script>
+	
+		
+</body>
+</html>
+
+(function(win, doc) {
+	$input = doc.querySelector('[data-js="input"]');
+	$buttonStart = doc.querySelector('[data-js="start"]');
+	$buttonStop = doc.querySelector('[data-js="stop"]');
+	$buttonReset = doc.querySelector('[data-js="reset"]');
+	
+	$input.value = 0;
+	
+	var temporizador;
+
+	$buttonStart.addEventListener('click', function () {
+		function startTimer () {
+			$input.value++;
+			
+			temporizador = setTimeout(startTimer, 1000);
+		}
+		
+		startTimer()
+		
+	}, false);
+	
+	$buttonStop.addEventListener('click', function() {
+		clearTimeout(temporizador);
+	}, false);
+	
+	$buttonReset.addEventListener('click', function() {
+		$input.value = 0;
+		clearTimeout(temporizador);
+	}, false);
+
+})(window, document);
